d745509b0408935e78937cf9f50940da90fbeb9c
thirstycode <pratikkinage@gmail.com>
2022-03-14 10:45:41 +0530

    block styles setup

--- functions.php
+++ functions.php
@@ -24,33 +24,29 @@ define( 'GRIGORA_VERSION', wp_get_theme()->get( 'Version' ) );
  */
 function grigora_setup() {
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+
+	add_theme_support( 'responsive-embeds' );
+
 	add_theme_support( 'wp-block-styles' );
 
-
 	add_theme_support( 'editor-styles' );
-		
 
-		// Enqueue editor styles and fonts.
+	// Enqueue editor styles and fonts.
 	add_editor_style(
 		array(
-			'./dist/editor.min.css'
+			'./dist/css/grigora-editor.min.css'
 		)
 	);
 
-	/*
-	 * Load additional block styles.
-	 * See details on how to add more styles in the readme.txt.
-	 */
-	$styled_blocks = [ 'button', 'file', 'latest-comments', 'latest-posts', 'post-title', 'quote', 'search'  ];
-	foreach ( $styled_blocks as $block_name ) {
-		$args = array(
-			'handle' => "grigora-$block_name",
-			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.min.css" ),
-			$args['path'] = get_theme_file_path( "assets/css/blocks/$block_name.min.css" ),
-		);
-		// Replace the "core" prefix if you are styling blocks from plugins.
-		wp_enqueue_block_style( "core/$block_name", $args );
-	}
+	add_theme_support('title-tag');
+	add_theme_support(
+		'html5',
+		[
+			'search-form',
+			'comment-form',
+			'comment-list',
+		]
+	);
 
 }
 add_action( 'after_setup_theme', 'grigora_setup' );
@@ -66,12 +62,6 @@ function grigora_styles() {
 	wp_enqueue_style(
 		'grigora-style',
 		get_theme_file_uri('dist/css/global.min.css'),
-		[],
-		GRIGORA_VERSION
-	);
-	wp_enqueue_style(
-		'grigora-shared-styles',
-		get_theme_file_uri( 'assets/css/style-shared.min.css' ),
 		[],
 		GRIGORA_VERSION
 	);
--- inc/register-block-styles.php
+++ inc/register-block-styles.php
@@ -31,93 +31,11 @@ function grigora_register_block_styles() {
 		)
 	);
 
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/list',
-		array(
-			'name'  => 'grigora-list-underline',
-			'label' => __( 'Underlined list items', 'grigora' ),
-		)
-	);
 
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/group',
-		array(
-			'name'  => 'grigora-box-shadow',
-			'label' => __( 'Box shadow', 'grigora' ),
-		)
-	);
-
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/column',
-		array(
-			'name'  => 'grigora-box-shadow',
-			'label' => __( 'Box shadow', 'grigora' ),
-		)
-	);
-
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/columns',
-		array(
-			'name'  => 'grigora-box-shadow',
-			'label' => __( 'Box shadow', 'grigora' ),
-		)
-	);
-
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/site-title',
-		array(
-			'name'  => 'grigora-text-shadow',
-			'label' => __( 'Text shadow', 'grigora' ),
-		)
-	);
-
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/post-title',
-		array(
-			'name'  => 'grigora-text-shadow',
-			'label' => __( 'Text shadow', 'grigora' ),
-		)
-	);
-
-	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
-		'core/heading',
-		array(
-			'name'  => 'grigora-text-shadow',
-			'label' => __( 'Text shadow', 'grigora' ),
-		)
-	);
-	register_block_style(
+	register_block_style(  // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
 		'core/query', [
 		'name' => 'grigora-horizontalquery',
 		'label' => __('Horizontal Query', 'grigora'),
 	]);
 }
 add_action( 'init', 'grigora_register_block_styles' );
-
-
-
-//Include conditional assets
-add_filter('render_block', 'grigora_theme_conditional_assets', 10, 2);
-function grigora_theme_conditional_assets($html, $block)
-{
-	$block_style = '';
-
-	if(!is_admin()){//prevent loading conditional assets in admin
-		//We use checking by class name until Wordpress will have proper inline style registration for block styles
-		if (isset($block['blockName'])) {
-			if (!empty($block['attrs']['className'])) {
-				if ($block['blockName'] == 'core/query') {
-					if (str_contains($block['attrs']['className'], 'is-style-grigora-horizontalquery') !== false) {
-						$block_style .= '.is-style-grigora-horizontalquery {background-color:#000}';
-					}
-				}
-			}
-		}
-
-		if ($block_style) {
-			$html =  $html . '<style scoped>' . $block_style . '</style>';
-		}
-		
-	}
-	return $html;
-}
