416813d9075b92f3c921f3c022b94a3827788726
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-03-21 11:49:11 +0530

    hero 1 minor changes

--- patterns/hero.php
+++ patterns/hero.php
@@ -7,30 +7,36 @@
  */
 
 return array(
-	'title'      => esc_html__( 'Hero', 'grigora-blocks' ),
+	'title'      => esc_html__( 'Hero-1', 'grigora-blocks' ),
 	'categories' => array( 'text' ),
 	'inserter'   => true,
 	'content'    => '
 	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"10%","right":"10%","bottom":"10%","left":"10%"}}},"gradient":"luminous-dusk"} -->
-	<div class="wp-block-group alignfull has-luminous-dusk-gradient-background has-background" style="padding-top:10%;padding-right:10%;padding-bottom:10%;padding-left:10%"><!-- wp:columns {"verticalAlignment":null} -->
+	<div class="wp-block-group alignfull has-luminous-dusk-gradient-background has-background" style="padding-top:10%;padding-right:10%;padding-bottom:10%;padding-left:10%"><!-- wp:columns -->
 	<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
 	<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textColor":"background"} -->
 	<h2 class="has-background-color has-text-color">Grigora Theme</h2>
 	<!-- /wp:heading -->
-	
+
 	<!-- wp:paragraph {"textColor":"background"} -->
 	<p class="has-background-color has-text-color">Highly Customizable</p>
 	<!-- /wp:paragraph -->
-	
+
 	<!-- wp:paragraph {"textColor":"background"} -->
 	<p class="has-background-color has-text-color">Unique and Responsive Blocks</p>
 	<!-- /wp:paragraph -->
-	
+
 	<!-- wp:paragraph {"textColor":"background"} -->
 	<p class="has-background-color has-text-color">Crazy Fast</p>
-	<!-- /wp:paragraph --></div>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:buttons -->
+	<div class="wp-block-buttons"><!-- wp:button {"textColor":"background","className":"is-style-grigora-flat-outline"} -->
+	<div class="wp-block-button is-style-grigora-flat-outline"><a class="wp-block-button__link has-background-color has-text-color">Download Now!</a></div>
+	<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
 	<!-- /wp:column -->
-	
+
 	<!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:image {"id":2306,"sizeSlug":"full","linkDestination":"none"} -->
 	<figure class="wp-block-image size-full"><img src="http://localhost/latracal/wp-content/uploads/2022/03/herobanner.webp" alt="" class="wp-image-2306"/></figure>
@@ -39,4 +45,5 @@ return array(
 	<!-- /wp:columns --></div>
 	<!-- /wp:group -->
 	',
+	
 );
