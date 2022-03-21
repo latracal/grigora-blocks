14bb035fadad5280e2e9f1e17a1f2e1934912a51
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-03-21 13:39:53 +0530

    full width

--- patterns/hero-2.php
+++ patterns/hero-2.php
@@ -11,8 +11,8 @@ return array(
 	'categories' => array( 'text' ),
 	'inserter'   => true,
 	'content'    => '
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"5%","right":"5%","bottom":"5%","left":"5%"}},"color":{"gradient":"linear-gradient(135deg,rgb(0,166,252) 0%,rgb(241,0,255) 95%)"}}} -->
-	<div class="wp-block-group has-background" style="background:linear-gradient(135deg,rgb(0,166,252) 0%,rgb(241,0,255) 95%);padding-top:5%;padding-right:5%;padding-bottom:5%;padding-left:5%"><!-- wp:columns {"className":"hero-block-2"} -->
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"5%","right":"5%","bottom":"5%","left":"5%"}},"color":{"gradient":"linear-gradient(135deg,rgb(0,166,252) 0%,rgb(241,0,255) 95%)"}}} -->
+	<div class="wp-block-group alignfull has-background" style="background:linear-gradient(135deg,rgb(0,166,252) 0%,rgb(241,0,255) 95%);padding-top:5%;padding-right:5%;padding-bottom:5%;padding-left:5%"><!-- wp:columns {"className":"hero-block-2"} -->
 	<div class="wp-block-columns hero-block-2"><!-- wp:column {"width":"30%","style":{"spacing":{"padding":{"bottom":"30px"}}},"backgroundColor":"background","className":"is-style-grigora-shadow"} -->
 	<div class="wp-block-column is-style-grigora-shadow has-background-background-color has-background" style="padding-bottom:30px;flex-basis:30%"><!-- wp:heading {"textAlign":"center"} -->
 	<h2 class="has-text-align-center">Heading / Title</h2>
