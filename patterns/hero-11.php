b9d5deed90febb579b9207b10b476829de6db10c
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-03-22 13:40:06 +0530

    hero-11 video removed

--- patterns/hero-11.php
+++ patterns/hero-11.php
@@ -11,10 +11,10 @@ return array(
 	'categories' => array( 'text' ),
 	'inserter'   => true,
 	'content'    => '
-	<!-- wp:cover {"url":"'.esc_url( get_theme_file_uri( 'assets/video/hero_video.mp4' ) ).'":"foreground","backgroundType":"video","focalPoint":{"x":"0.50","y":0.5},"isDark":false,"style":{"spacing":{"padding":{"top":"8%","right":"8%","bottom":"8%","left":"8%"}}}} -->
-	<div class="wp-block-cover is-light" style="padding-top:8%;padding-right:8%;padding-bottom:8%;padding-left:8%"><span aria-hidden="true" class="has-foreground-background-color has-background-dim-20 wp-block-cover__gradient-background has-background-dim"></span><video class="wp-block-cover__video-background intrinsic-ignore" autoplay muted loop playsinline src="http://localhost/latracal/wp-content/uploads/2022/03/hero_video.mp4" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"></video><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"top"} -->
+	<!-- wp:cover {"dimRatio":20,"overlayColor":"foreground","contentPosition":"center center","style":{"spacing":{"padding":{"top":"8%","right":"8%","bottom":"8%","left":"8%"}}}} -->
+	<div class="wp-block-cover" style="padding-top:8%;padding-right:8%;padding-bottom:8%;padding-left:8%"><span aria-hidden="true" class="has-foreground-background-color has-background-dim-20 wp-block-cover__gradient-background has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"top"} -->
 	<div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:columns {"verticalAlignment":null} -->
+	<div class="wp-block-column"><!-- wp:columns -->
 	<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top"} -->
 	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"textAlign":"center","textColor":"background","fontSize":"x-large"} -->
 	<h2 class="has-text-align-center has-background-color has-text-color has-x-large-font-size">Get exclusive travel tips</h2>
