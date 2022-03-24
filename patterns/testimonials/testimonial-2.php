43d4a1a870f7bfef6108cc5e623aa5932c520c6b
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-03-24 14:30:27 +0530

    testimonial 2 added

--- patterns/testimonials/testimonial-2.php
+++ patterns/testimonials/testimonial-2.php
@@ -12,39 +12,47 @@ return array(
 	'inserter'   => true,
 	'content'    => '
 	<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"top":"5%","right":"5%","bottom":"5%","left":"5%"}}}} -->
-	<div class="wp-block-columns alignfull" style="padding-top:5%;padding-right:5%;padding-bottom:5%;padding-left:5%"><!-- wp:column {"backgroundColor":"background","className":"grigora-testimonial-2 is-style-default"} -->
-	<div class="wp-block-column grigora-testimonial-2 is-style-default has-background-background-color has-background"><!-- wp:columns {"className":"p-3 mb-0 bt-5"} -->
-	<div class="wp-block-columns p-3 mb-0 bt-5"><!-- wp:column {"verticalAlignment":"top"} -->
-	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:quote -->
-	<blockquote class="wp-block-quote"><p>If you do the job badly enough, sometimes you don\'t get asked to do it again.</p><cite>Wisteria Ravenclaw</cite></blockquote>
-	<!-- /wp:quote --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:column -->
+<div class="wp-block-columns alignfull" style="padding-top:5%;padding-right:5%;padding-bottom:5%;padding-left:5%"><!-- wp:column {"backgroundColor":"background","className":"grigora-testimonial-2 is-style-default"} -->
+<div class="wp-block-column grigora-testimonial-2 is-style-default has-background-background-color has-background"><!-- wp:columns {"className":"p-3 mb-0 bt-5"} -->
+<div class="wp-block-columns p-3 mb-0 bt-5"><!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
+<blockquote class="wp-block-quote" style="font-style:normal;font-weight:400"><p>If you do the job badly enough, sometimes you don\'t get asked to do it again.</p></blockquote>
+<!-- /wp:quote -->
 
-	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
-	<div class="wp-block-column" style="flex-basis:100%"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-	<div class="wp-block-columns" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"width":"100%"} -->
-	<div class="wp-block-column" style="flex-basis:100%"></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:column -->
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<h2 class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400">Wisteria Ravenclaw</h2>
+<!-- /wp:heading -->
 
-	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
-	<div class="wp-block-column" style="flex-basis:100%"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-	<div class="wp-block-columns" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"width":"100%"} -->
-	<div class="wp-block-column" style="flex-basis:100%"></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns -->
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"color":{"text":"#9e9e9e"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"extra-small"} -->
+<h3 class="has-text-align-center has-text-color has-extra-small-font-size" style="color:#9e9e9e;font-style:normal;font-weight:400">Google Inc.</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
 	',	
 );
