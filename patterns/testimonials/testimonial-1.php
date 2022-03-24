e3d4c4dc8437aaadda9affa29d5d2e9e59925bf0
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-03-24 12:44:11 +0530

    changes made testimonial 1

--- patterns/testimonials/testimonial-1.php
+++ patterns/testimonials/testimonial-1.php
@@ -17,8 +17,8 @@ return array(
 	<!-- /wp:column -->
 
 	<!-- wp:column {"className":"grigora-testimonial"} -->
-	<div class="wp-block-column grigora-testimonial"><!-- wp:cover {"url":"http://localhost/latracal/wp-content/uploads/2022/03/sq-sample40.jpeg","id":2491,"dimRatio":50,"style":{"spacing":{"padding":{"top":"3rem","right":"3rem","bottom":"3rem","left":"3rem"}}}} -->
-	<div class="wp-block-cover" style="padding-top:3rem;padding-right:3rem;padding-bottom:3rem;padding-left:3rem"><span aria-hidden="true" class="wp-block-cover__gradient-background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2491" alt="" src="http://localhost/latracal/wp-content/uploads/2022/03/sq-sample40.jpeg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:quote {"className":"is-style-default","style":{"typography":{"fontSize":"1.5rem"}}} -->
+	<div class="wp-block-column grigora-testimonial"><!-- wp:cover {"url":"https://c.pxhere.com/photos/09/d3/light_music_crowd_concert_festival-145352.jpg!d","id":2491,"dimRatio":50,"style":{"spacing":{"padding":{"top":"3rem","right":"3rem","bottom":"3rem","left":"3rem"}}}} -->
+	<div class="wp-block-cover" style="padding-top:3rem;padding-right:3rem;padding-bottom:3rem;padding-left:3rem"><span aria-hidden="true" class="wp-block-cover__gradient-background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2491" alt="" src="https://c.pxhere.com/photos/09/d3/light_music_crowd_concert_festival-145352.jpg!d" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:quote {"className":"is-style-default","style":{"typography":{"fontSize":"1.5rem"}}} -->
 	<blockquote class="wp-block-quote is-style-default" style="font-size:1.5rem"><p>Which is worse, that everyone has his price, or that the price is always so low.</p><cite>SUE SHEI</cite></blockquote>
 	<!-- /wp:quote --></div></div>
 	<!-- /wp:cover --></div>
