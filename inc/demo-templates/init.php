a785a83eca64dafa7aa2dbd7fe24bee18ac46a9d
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-04-08 11:27:21 +0530

    demo tempalate ui

--- inc/demo-templates/init.php
+++ inc/demo-templates/init.php
@@ -39,15 +39,24 @@ if(!function_exists("render_templates_html")){
         $json = file_get_contents( get_theme_file_path( 'inc/demo-templates/templates-meta/meta.json' ));
         $json = json_decode($json, true);
         echo '<h1>'.esc_html( __( "Import Demo Templates", "grigora-blocks-pro" )).'</h1>';
+        echo '<div class="template-search">';
+        echo '<input type="search" name="search" placeholder="Search...">';
+        echo '<select>';
+        echo '<option value="All">All</option>';
+        echo '</select>';
+        echo '</div>';
         echo '<div class="grigora-templates">';
     
         foreach ($json as $template => $template_meta) {
             echo '<div class="grigora-templates-single">';
             echo '<div class="grigora-screenshot">';
-            echo '<img src="'.get_theme_file_uri('assets/images/demo-templates/'.$template_meta['screenshot']).'" alt="'.$template_meta['name'].'" width="600" height="600">';
+            echo '<img src="'.get_theme_file_uri('assets/images/demo-templates/'.$template_meta['screenshot']).'" alt="'.$template_meta['name'].'" width="300" height="300">';
             echo '</div>';
             echo '<div class="grigora-template-name">';
             echo $template_meta['name'];
+            echo '</div>';
+            echo '<div class="demo-overlay">';
+            echo '<a href="#">View Demo</a>';
             echo '</div>';
             echo '</div>';
         }
