fba01e3ae32716c922cdafc8dc0a7ea592c5911f
thirstycode <pratikkinage@gmail.com>
2022-04-09 23:01:25 +0530

    template import logics via wp_admin post request

--- inc/demo-templates/import.php
+++ inc/demo-templates/import.php
@@ -1,5 +1,66 @@
 <?php
 
+/**
+ * Get Filesystem
+*/
+
+if(!function_exists("get_filesystem")){
+    function get_filesystem() {
+        global $wp_filesystem;
+    
+        // If the filesystem has not been instantiated yet, do it here.
+        if ( ! $wp_filesystem ) {
+            if ( ! function_exists( 'WP_Filesystem' ) ) {
+                require_once wp_normalize_path( ABSPATH . '/wp-admin/includes/file.php' );
+            }
+            WP_Filesystem();
+        }
+        return $wp_filesystem;
+    }
+}
+
+/**
+ * Get Base Path
+*/
+if(!function_exists("get_base_path")){
+    function get_base_path(){
+        return get_filesystem()->wp_content_dir();
+    }
+}
+
+/**
+ * Get Base Path
+*/
+if(!function_exists("get_theme_dir_full")){
+    function get_theme_dir_full(){
+        return get_base_path()."themes/grigora-blocks/";
+    }
+}
+
+if(!function_exists("write_file_to_contents")){
+    function write_file_to_contents($filename, $contents, $template){
+        if ( ! defined( 'FS_CHMOD_DIR' ) ) {
+            define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) );
+        }
+        if($template){
+            $html_folder = get_theme_dir_full() . "templates/";
+        }
+        else{
+            $html_folder = get_theme_dir_full() . "parts/";
+        }
+        if ( ! file_exists( $html_folder ) ) {
+            if(!get_filesystem()->mkdir( $html_folder, FS_CHMOD_DIR )){
+                return false;
+            }
+        }
+        if(!get_filesystem()->put_contents( $html_folder.$filename, $contents) ) {
+            return false;
+        }
+        return true;
+    }
+}
+
+
 add_action( 'admin_post_grigora_template_import', 'grigora_template_import' );
 
 /**
@@ -13,16 +74,26 @@ if(!function_exists("grigora_template_import")){
     function grigora_template_import() {
         if( isset( $_POST["_wpnonce"]) && $_POST["_wpnonce"] && $_POST["template"] ) {
             if ( ! wp_verify_nonce( $_POST["_wpnonce"], 'grigora_template_import' ) ) {
-                wp_die( __( 'The link you followed has expired.', 'grigora-blocks-pro' ) ); 
+                wp_die( __( 'The link you followed has expired.', 'grigora-blocks' ) ); 
             } else {
                 $template = $_POST["template"];
                 $json = file_get_contents( get_theme_file_path( 'inc/demo-templates/templates-meta/meta.json' ) );
                 $json = json_decode($json, true);
                 if(array_key_exists($template, $json)){
-    
-                }
+                    $existing_files = get_filesystem().dirlist(get_theme_dir_full()."templates/");
+                    $new_files = get_filesystem().dirlist(get_theme_dir_full()."inc/demo-templates/templates/". $template. "/templates". "/" );
+                    foreach($existing_files as $existing_file){
+                        if(!in_array($existing_file, $new_files)){
+                            get_filesystem()->delete( get_theme_dir_full() . "templates/" . $existing_file);
+                        }
+                    }
+                    foreach($new_files as $new_file){
+                        $contents = get_filesystem()->get_contents( get_theme_dir_full()."inc/demo-templates/templates/". $template. "/templates". "/". $new_file);
+                        write_file_to_contents($new_file, $contents, 1);
+                    }
+                }   
                 else{
-                    wp_die( __( 'Incorrect template slug.', 'grigora-blocks-pro' ) ); 
+                    wp_die( __( 'Incorrect template slug.', 'grigora-blocks' ) ); 
                 }
             }
             wp_redirect(admin_url('admin.php?page=grigora-templates'));
@@ -30,5 +101,3 @@ if(!function_exists("grigora_template_import")){
         wp_redirect(admin_url('admin.php?page=grigora-templates'));
     }
 }
-
-
--- inc/demo-templates/init.php
+++ inc/demo-templates/init.php
@@ -4,7 +4,7 @@ require_once get_theme_file_path( 'inc/demo-templates/import.php' );
 
 if ( ! function_exists( 'gbp_options_menu' ) ) { 
     function gbp_options_menu() {
-        add_theme_page( 'grigora-templates', __('Grigora Demo Templates', 'grigora-blocks-pro'), 'manage_options', 'grigora-templates', 'grigora_templates_page' );
+        add_theme_page( 'grigora-templates', __('Grigora Demo Templates', 'grigora-blocks'), 'manage_options', 'grigora-templates', 'grigora_templates_page' );
     }
 }
 
@@ -12,7 +12,7 @@ if(!function_exists("grigora_templates_page")){
     function grigora_templates_page() {
 
         if ( !current_user_can( 'manage_options' ) ) {
-            wp_die( __( 'You do not have sufficient permissions to access this page.' , 'grigora-blocks-pro') );
+            wp_die( __( 'You do not have sufficient permissions to access this page.' , 'grigora-blocks') );
         }
         echo '<div class="admin-container">';
         settings_errors();
@@ -38,11 +38,22 @@ if(!function_exists("render_templates_html")){
 
         $json = file_get_contents( get_theme_file_path( 'inc/demo-templates/templates-meta/meta.json' ));
         $json = json_decode($json, true);
-        echo '<h1>'.esc_html( __( "Import Demo Templates", "grigora-blocks-pro" )).'</h1>';
+        $unique_categories = array();
+        foreach ($json as $template => $template_meta) {
+            foreach($template_meta['category'] as $category){
+                if(!in_array($category, $unique_categories)){
+                    array_push($unique_categories, $category);
+                }
+            }
+        }
+        echo '<h1>'.esc_html( __( "Import Demo Templates", "grigora-blocks" )).'</h1>';
         echo '<div class="template-search">';
-        echo '<input type="search" name="search" placeholder="Search...">';
-        echo '<select>';
+        echo '<input type="search" name="search" id="grigora-templates-input" placeholder="Search...">';
+        echo '<select id="grigora-templates-select">';
         echo '<option value="All">All</option>';
+        foreach ($unique_categories as $category) {
+            echo '<option value="'.$category.'">'.$category.'</option>';
+        }
         echo '</select>';
         echo '</div>';
         echo '<div class="grigora-templates">';
@@ -78,12 +89,15 @@ if(!function_exists("render_templates_html")){
         echo '</div>';
         echo '</div>';
         echo '<div class="modal-iframe-div">';
-        echo '<iframe src="#" title="Title" class="modal-iframe"></iframe>';
+        echo '<iframe src="" title="Title" class="modal-iframe"></iframe>';
         echo '</div>';
         echo '</div>';       
     
         foreach ($json as $template => $template_meta) {
-            echo '<div class="grigora-templates-single">';
+            echo '<div class="grigora-templates-single"
+            data-name="'.$template_meta['name'].'" 
+            data-category=\''.json_encode($template_meta['category']).'\' 
+            >';
             echo '<div class="grigora-screenshot">';
             echo '<img src="'.get_theme_file_uri('assets/images/demo-templates/'.$template_meta['screenshot']).'" alt="'.$template_meta['name'].'" width="300" height="300">';
             echo '</div>';
