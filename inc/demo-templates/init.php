7c5790fb918d1f7652ede9fe23ef15592037b4c3
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-04-13 11:48:51 +0530

    confirmation modal added but the script is not working

--- inc/demo-templates/init.php
+++ inc/demo-templates/init.php
@@ -86,10 +86,17 @@ if(!function_exists("render_templates_html")){
         echo '<input type="hidden" name="template" class="form-template" value="blog_pro">';
         wp_nonce_field( 'grigora_template_import', '_grigora_template_import' );
         echo '<div class="modal_install_btn">';
-        echo '<button type="submit" value="Submit" class="btn-install">Install<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+        echo '<p class="btn-install">Install<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
             <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-          </svg></button>';
+          </svg></p>';
+        echo '</div>';
+        echo '<div class="confirm-modal open">';
+        echo '<p>Are you sure?</p>';
+        echo '<div class="confirm-btn">';
+        echo '<p class="close-btn">Cancel</p>';
+        echo '<button type="submit" value="Submit">Yes</button>';
+        echo '</div>';
         echo '</div>';
         echo '</form>';
         echo '</div>';
--- js/admin.js
+++ js/admin.js
@@ -14,8 +14,8 @@ var toggleModal = function (event) {
 	var iframe = modal.querySelector(".modal-iframe");
 	iframe.setAttribute("src", demo_url);
 	iframe.setAttribute("title", name);
-  var formtemplate = modal.querySelector(".form-template");
-  formtemplate.value = slug;
+	var formtemplate = modal.querySelector(".form-template");
+	formtemplate.value = slug;
 	modal.classList.add("open-modal");
 };
 
@@ -36,104 +36,112 @@ Array.from(modalClose).forEach(function (element) {
 });
 
 var svg = document.querySelector(".modal_screen_size");
-if(svg){
-  var icon = svg.getElementsByClassName("bi");
-  if(icon){
-    for (var i = 0; i < icon.length; i++) {
-      icon[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].classList = current[0].classList.add("active");
-        this.classList += " active";
-      });
-    }
-  }
+if (svg) {
+	var icon = svg.getElementsByClassName("bi");
+	if (icon) {
+		for (var i = 0; i < icon.length; i++) {
+			icon[i].addEventListener("click", function () {
+				var current = document.getElementsByClassName("active");
+				current[0].classList = current[0].classList.add("active");
+				this.classList += " active";
+			});
+		}
+	}
 }
-
 
 const desktopView = document.querySelector(".bi-display");
 const tabletView = document.querySelector(".bi-tablet-landscape");
 const mobileView = document.querySelector(".bi-phone");
 const iframe = document.querySelector(".modal-iframe");
 
-if(desktopView){
-  desktopView.addEventListener("click", function () {
-    if (iframe.classList.contains("tablet")) {
-      iframe.classList.remove("tablet");
-    }
-    if (iframe.classList.contains("mobile")) {
-      iframe.classList.remove("mobile");
-    }
-    iframe.classList.add("desktop");
-  });
+if (desktopView) {
+	desktopView.addEventListener("click", function () {
+		if (iframe.classList.contains("tablet")) {
+			iframe.classList.remove("tablet");
+		}
+		if (iframe.classList.contains("mobile")) {
+			iframe.classList.remove("mobile");
+		}
+		iframe.classList.add("desktop");
+	});
 }
 
-if(tabletView){
-  tabletView.addEventListener("click", function () {
-    if (iframe.classList.contains("desktop")) {
-      iframe.classList.remove("desktop");
-    }
-    if (iframe.classList.contains("mobile")) {
-      iframe.classList.remove("mobile");
-    }
-    iframe.classList.add("tablet");
-  });
+if (tabletView) {
+	tabletView.addEventListener("click", function () {
+		if (iframe.classList.contains("desktop")) {
+			iframe.classList.remove("desktop");
+		}
+		if (iframe.classList.contains("mobile")) {
+			iframe.classList.remove("mobile");
+		}
+		iframe.classList.add("tablet");
+	});
 }
 
-if(mobileView){
-  mobileView.addEventListener("click", function () {
-    if (iframe.classList.contains("desktop")) {
-      iframe.classList.remove("desktop");
-    }
-    if (iframe.classList.contains("tablet")) {
-      iframe.classList.remove("tablet");
-    }
-    iframe.classList.add("mobile");
-  });
+if (mobileView) {
+	mobileView.addEventListener("click", function () {
+		if (iframe.classList.contains("desktop")) {
+			iframe.classList.remove("desktop");
+		}
+		if (iframe.classList.contains("tablet")) {
+			iframe.classList.remove("tablet");
+		}
+		iframe.classList.add("mobile");
+	});
 }
 
-
-
-var sel = document.getElementById('grigora-templates-select');
+var sel = document.getElementById("grigora-templates-select");
 var singles = document.getElementsByClassName("grigora-templates-single");
 
-if(sel){
-  sel.onchange = function(e) {
-    var selectedvalue = this[this.selectedIndex].text;
-    for (let i = 0; i < singles.length; i++) {
-      const element = singles[i];
-      var categories = JSON.parse(element.getAttribute("data-category"));
-      if(selectedvalue === "All"){
-        element.classList.remove("cat_filter");
-      }
-      else if (!categories.includes(selectedvalue)){
-        element.classList.add("cat_filter");
-      }
-      else{
-        element.classList.remove("cat_filter");
-      }
-    }  
-  };
+if (sel) {
+	sel.onchange = function (e) {
+		var selectedvalue = this[this.selectedIndex].text;
+		for (let i = 0; i < singles.length; i++) {
+			const element = singles[i];
+			var categories = JSON.parse(element.getAttribute("data-category"));
+			if (selectedvalue === "All") {
+				element.classList.remove("cat_filter");
+			} else if (!categories.includes(selectedvalue)) {
+				element.classList.add("cat_filter");
+			} else {
+				element.classList.remove("cat_filter");
+			}
+		}
+	};
 }
 
+const inputsrc = document.getElementById("grigora-templates-input");
 
+const inputHandler = function (e) {
+	var textinput = e.target.value;
+	for (let i = 0; i < singles.length; i++) {
+		const element = singles[i];
+		var name = element.getAttribute("data-name");
+		if (name.toLowerCase().includes(textinput.toLowerCase())) {
+			element.classList.remove("text_filter");
+		} else {
+			element.classList.add("text_filter");
+		}
+	}
+};
 
-const inputsrc = document.getElementById('grigora-templates-input');
-
-const inputHandler = function(e) {
-  var textinput = e.target.value;
-  for (let i = 0; i < singles.length; i++) {
-    const element = singles[i];
-    var name = element.getAttribute("data-name");
-    if(name.toLowerCase().includes(textinput.toLowerCase())){
-      element.classList.remove("text_filter");
-    }
-    else{
-      element.classList.add("text_filter");
-    }
-  }
+if (inputsrc) {
+	inputsrc.addEventListener("input", inputHandler);
+	inputsrc.addEventListener("propertychange", inputHandler);
 }
 
-if(inputsrc){
-  inputsrc.addEventListener('input', inputHandler);
-  inputsrc.addEventListener('propertychange', inputHandler);
-}
+const conBtn = document.querySelector(".btn-install");
+const conModal = document.querySelector(".confirm-modal");
+const closeBtn = document.querySelector(".close-btn");
+
+var toggleConModalClose = function (event) {
+	if (conModal.classList.contains("open")) {
+		conModal.classList.remove("open");
+	}
+	var target = event.target;
+	conModal.classList.remove("open");
+};
+
+Array.from(closeBtn).forEach(function (element) {
+	element.addEventListener("click", toggleConModalClose);
+});
